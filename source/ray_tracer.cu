ab21779e864cba7c632c4e107a07c47fc77f9909
John Novak <janovak@mtu.edu>
2024-06-24 07:22:21 -0700

    Add vec3 and color classes

--- source/ray_tracer.cu
+++ source/ray_tracer.cu
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "color.h"
+
 int main() {
 
     // Image
@@ -11,17 +13,13 @@ int main() {
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = 0; j < image_height; j++) {
+    for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            color pixel_color = color(static_cast<double>(i)/(image_width-1), static_cast<double>(j)/(image_height-1), 0);
+            std::cout << pixel_color;
         }
     }
+
+    std::clog << "\rDone.                 \n";
 }
---
+++ source/vec3.h
@@ -0,0 +1,110 @@
+#pragma once
+
+#include <cuda_runtime.h>
+#include <iostream>
+
+template <typename T>
+class vec3_base {
+  public:
+    double e[3];
+
+    __host__ __device__ vec3_base() : e{0,0,0} {}
+    __host__ __device__ vec3_base(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+    __host__ __device__ double x() const { return e[0]; }
+    __host__ __device__ double y() const { return e[1]; }
+    __host__ __device__ double z() const { return e[2]; }
+
+    __host__ __device__ vec3_base<T> operator-() const { return vec3_base<T>(-e[0], -e[1], -e[2]); }
+    __host__ __device__ double operator[](int i) const { return e[i]; }
+    __host__ __device__ double& operator[](int i) { return e[i]; }
+
+    __host__ __device__ vec3_base<T>& operator+=(const vec3_base<T>& v) {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this;
+    }
+
+    __host__ __device__ vec3_base<T>& operator*=(double t) {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
+
+    __host__ __device__ vec3_base<T>& operator/=(double t) {
+        return *this *= 1/t;
+    }
+
+    template<typename U = T, typename std::enable_if<!std::is_same<U, void>::value>::type* = nullptr>
+    __host__ __device__ operator T() const {
+        return T(e[0], e[1], e[2]);
+    }
+
+    __host__ __device__ double length() const {
+        return sqrt(length_squared());
+    }
+
+    __host__ __device__ double length_squared() const {
+        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
+};
+
+using vec3 = vec3_base<void>;
+
+// Vector Utility Functions
+
+template <typename T>
+inline std::ostream& operator<<(std::ostream& out, const vec3_base<T>& v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+template <typename T>
+inline vec3_base<T> operator+(const vec3_base<T>& u, const vec3_base<T>& v) {
+    return vec3_base<T>(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+template <typename T>
+inline vec3_base<T> operator-(const vec3_base<T>& u, const vec3_base<T>& v) {
+    return vec3_base<T>(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+template <typename T>
+inline vec3_base<T> operator*(const vec3_base<T>& u, const vec3_base<T>& v) {
+    return vec3_base<T>(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+template <typename T>
+inline vec3_base<T> operator*(double t, const vec3_base<T>& v) {
+    return vec3_base<T>(t*v.e[0], t*v.e[1], t*v.e[2]);
+}
+
+template <typename T>
+inline vec3_base<T> operator*(const vec3_base<T>& v, double t) {
+    return t * v;
+}
+
+template <typename T>
+inline vec3_base<T> operator/(const vec3_base<T>& v, double t) {
+    return (1/t) * v;
+}
+
+template <typename T>
+inline double dot(const vec3_base<T>& u, const vec3_base<T>& v) {
+    return u.e[0] * v.e[0]
+         + u.e[1] * v.e[1]
+         + u.e[2] * v.e[2];
+}
+
+template <typename T>
+inline vec3_base<T> cross(const vec3_base<T>& u, const vec3_base<T>& v) {
+    return vec3_base<T>(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                        u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                        u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+template <typename T>
+inline vec3_base<T> unit_vector(const vec3_base<T>& v) {
+    return v / v.length();
+}
